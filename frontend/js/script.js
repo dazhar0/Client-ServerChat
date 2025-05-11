afbae23c1baa8bfb69c80987d91d9db306b5390e
Daniyal Azhar <daniyalazhar@MacBookPro.lan>
2025-05-10 19:04:39 -0700

    fixing websocket issue

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,6 @@
 let socket;
 let username = localStorage.getItem("username") || "";
-const serverUrl = "wss://client-serverchat.onrender.com/ws"; // Replace with your Render WebSocket URL
+const serverUrl = "wss://client-serverchat.onrender.com"; // Replace with your Render WebSocket URL
 
 function connectWebSocket() {
   if (!username) {
